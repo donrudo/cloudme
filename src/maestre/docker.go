d8af8b23a73625e1096edf42f73c2cc9b70b9f18
Carlos A. Morales <carlosalbertomr@gmail.com>
2015-09-24 22:18:38 -0700

    * Fixed loop when DOCKER_HOST is empty; now it will crash (TODO: not perfect, I know) * Began Run step implementation. * Added Channel map, it will track the finishing of each step executed inside of go rutines.

--- src/maestre/docker.go
+++ src/maestre/docker.go
@@ -3,12 +3,12 @@ package maestre
 // maestre module for the Docker Runtime actions
 
 import ( // Standard deps
-
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"time"
 )
 
 import ( // internal or vendor deps
@@ -22,6 +22,11 @@ type DockerRuntime struct {
 	Context string
 	Api     *DockerAPI.DockerClient
 	Debug   dbg.DebugFunction
+
+	// Builded instead of Built to be less confusing with "Build" function
+	Builded  map[string]chan bool
+	Started  map[string]chan bool
+	Verified map[string]chan bool
 }
 
 func NewDockerClient(context string) *DockerRuntime {
@@ -30,6 +35,9 @@ func NewDockerClient(context string) *DockerRuntime {
 	Docker.Debug = dbg.Debug("Docker Runtime")
 	Docker.Debug("Configuring Docker Runtime")
 	tlsConfig := &tls.Config{}
+	Docker.Builded = make(map[string]chan bool)
+	Docker.Started = make(map[string]chan bool)
+	Docker.Verified = make(map[string]chan bool)
 	var err error
 
 	if os.Getenv("DOCKER_TLS_VERIFY") != "" && os.Getenv("DOCKER_TLS_VERIFY") != "0" {
@@ -50,17 +58,43 @@ func NewDockerClient(context string) *DockerRuntime {
 		tlsConfig = nil
 	}
 
-	Docker.Api, err = DockerAPI.NewDockerClient(os.Getenv("DOCKER_HOST"), tlsConfig)
-	if err != nil {
-		fmt.Println(err)
+	if os.Getenv("DOCKER_HOST") != "" {
+		Docker.Api, err = DockerAPI.NewDockerClient(os.Getenv("DOCKER_HOST"), tlsConfig)
+		Docker.Debug("Ready Docker Runtime")
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
 	}
 
-	Docker.Debug("Ready Docker Runtime")
 	return Docker
 }
 
 func (dr DockerRuntime) Run(service config.Mservices, app config.App) {
+	dr.Started[service.Name] = make(chan bool)
+	for dr.Builded[service.Image] == nil {
+		time.Sleep(500 * time.Millisecond)
+	}
 	dr.Debug("Wait for Build to Run container: %s", service.Name)
+	done := false
+	BuildOk := false
+	for !done {
+		//wait until a message is received from the Builded[service.Image] channel
+		select {
+		case BuildOk = <-dr.Builded[service.Image]:
+			done = true
+		default:
+			time.Sleep(500 * time.Millisecond)
+			dr.Debug("RUN - waiting for Build(%s) to finish", service.Image)
+			done = false
+		}
+	}
+
+	if !BuildOk {
+		dr.Debug("RUN - Build(%s) failed", service.Image)
+		dr.Started[service.Name] <- false
+		return
+	}
 }
 
 // Verify uses the healthcheck set at the given service to put a message into the channel once it's correctly running or if an error is detected (timeouts at 30 secs)
@@ -69,15 +103,20 @@ func (dr DockerRuntime) Verify(service config.Mservices, app config.App) {
 }
 
 func (dr DockerRuntime) Build(service config.Mservices, app config.App) {
+	dr.Builded[service.Image] = make(chan bool)
 	dockerBuildContextName, err := dr.CreateTar(dr.Context)
 	if err != nil {
 		dr.Debug("%s", err)
 		fmt.Println(err)
+		dr.Builded[service.Image] <- false
+		return
 	}
 	dockerBuildContext, err := os.Open(dockerBuildContextName)
 	if err != nil {
 		dr.Debug("%s", err)
 		fmt.Println(err)
+		dr.Builded[service.Image] <- false
+		return
 	}
 
 	config := &DockerAPI.ConfigFile{
@@ -103,8 +142,12 @@ func (dr DockerRuntime) Build(service config.Mservices, app config.App) {
 	} else {
 		if b, err := ioutil.ReadAll(reader); err == nil {
 			dr.Debug("Build output: %s", string(b))
+			dr.Builded[service.Image] <- true
+			return
 		} else {
 			fmt.Println(err)
+			dr.Builded[service.Image] <- false
+			return
 		}
 	}
 }
